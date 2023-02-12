c057a3e8da4eb5ef7a27654d462096453db593fd
Jessica Benson <jess.nicole.benson@gmail.com>
2023-02-12 00:43:51 -0500

    FINAL

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 function greet() {
-    prompt ("What is your name?")
-
-    let sign = prompt ("Hello, Name");
+    var name = prompt ('What is your name?');
+    alert ('Hello, ' + name);
+   
+    var age = prompt ('How old are you?');
+    console.log(parseInt(age));
+   
+    var bday = confirm ("Have you had a birthday this year? OK for yes. Cancel for No.");
+   
+    if (bday === true) {
+        alert ("You were born in " + (new Date().getFullYear() - age))
+    } else if (bday === false) {
+        alert ("You were born in " + (new Date().getFullYear() - age - 1))
+    };
 }
-
-// hello 
